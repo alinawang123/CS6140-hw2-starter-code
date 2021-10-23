1455f8939f5de4cbde89192261c44e6508e7f580
Aanchan Mohan <aanchan@synaptitude.info>
2021-10-23 14:46:27 -0700

    Updated test code

--- test/test_stats.py
+++ test/test_stats.py
@@ -1,10 +1,10 @@
 import pytest
 import numpy as np
-from src.stats import calculate_mean
+from src.stats import calculate_mean, calculate_cov
 
 @pytest.fixture
 def sample_data():
-    x = np.rand(3,3) # You can change this sample data should you wish to
+    x = np.random.rand(5,3) # You can change this sample data should you wish to
     yield x
 
 @pytest.fixture
@@ -23,4 +23,6 @@ def test_mean(sample_data):
 
 
 def test_cov(sample_data, mean_of_data):
-    NotImplemented
+    cov_mat = calculate_cov(sample_data, mean_of_data)
+    cov = np.cov(sample_data, rowvar=False)
+    assert np.allclose(cov_mat, cov)
